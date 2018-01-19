525f98c866520630357cc3e3e6fff2d7d8f79775
dinbrca <dinbrca@gmail.com>
2018-01-19 03:02:15 +0200

    Update Search.php
    No reason for limiting to page 11 and so on

--- src/ApaiIO/Operations/Search.php
+++ src/ApaiIO/Operations/Search.php
@@ -128,7 +128,7 @@ class Search extends AbstractOperation
     public function setPage($page)
     {
         if (false === is_numeric($page) || $page < 1) {
-            throw new \InvalidArgumentException(sprintf('%s is an invalid page value. It has to be numeric, positive and between 1 and 10',
+            throw new \InvalidArgumentException(sprintf('%s is an invalid page value. It has to be numeric, positive and starting from 1',
                     $page));
         }
 
